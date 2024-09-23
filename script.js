530e6df1fe9ba746a29159dfe4b204f0d94f36ae
SadiaAfrinMim <mimsadia367@gmail.com>
2024-09-23 15:05:41 +0600

    history btn added

--- script.js
+++ script.js
@@ -7,16 +7,20 @@ function getInputTextField(id){
     const anyInnerTextParseFloat = parseFloat(anyInnerText)
     return anyInnerTextParseFloat
 }
-addMoneyArray =[]
-addMoneyArraySecond = []
+let addMoneyArrayFrist =[]
+let addMoneyArraySecond = []
+let addMoneyArrayThird = []
 
-function allAroundDonationFunction(inputID, availableMoneyID, addMoneyID, event) {
+function allAroundDonationFunction(inputID, availableMoneyID, addMoneyID,historyCommentID, event,addMoneyArray) {
   event.preventDefault(); 
 
   const inputValue = getInputFieldID(inputID);  
   console.log("Input Value Type:", typeof inputValue);
 
-  addMoneyArray.push(inputValue); 
+  addMoneyArray.push(inputValue);
+  if(isNaN(inputValue)||inputValue<=0){
+    return alert("Invalied Donate Amount")
+  } 
   let total = 0
       for(eachmoneyDonation of addMoneyArray){
           total+=eachmoneyDonation
@@ -29,67 +33,54 @@ function allAroundDonationFunction(inputID, availableMoneyID, addMoneyID, event)
   
 
   const availAbleMoney = getInputTextField(availableMoneyID); 
-  const updateMoney = availAbleMoney - total;  
-  console.log("Updated Available Money:", updateMoney);
-
-
+  const updateMoney = availAbleMoney - total; 
   
-  // const hisoryitem = document.createElement('div')
-  // hisoryitem.classList.add('shadow-lg','p-4')
-  // hisoryitem.innerHTML= 
-  // `   <p class= text-xl>
-  //             ${inputID}  Taka is Donated for famine-2024 at Feni, Bangladesh 
-  //           </p>
-  //           <p>Date : ${day}-${month}-${year} ${hours}:${minutes}:${seconds} (Bangladesh Standard Time)</p>`
-  //          const historycontainer = document.getElementById('history-section')
-  //          console.log(hisoryitem.innerHTML);
-  //          historycontainer.insertBefore(hisoryitem, historycontainer.firstChild);
+
+  console.log("Updated Available Money:", updateMoney);
+  document.getElementById(addMoneyID).innerText = total.toFixed(2);  
+  document.getElementById(availableMoneyID).innerText = updateMoney.toFixed(2); 
+  document.getElementById('history-list').classList.add('hidden')
+
+// comment section
+const historyComment = document.getElementById(historyCommentID).innerText
+
+
+  const date = new Date()
+  const hisoryitem = document.createElement('div')
+  hisoryitem.className ='shadow-lg p-4 space-y-4 rounded-md'
+  hisoryitem.innerHTML= 
+  `   <p class= text-xl>
+              ${inputValue.toFixed(2)} Tk is ${historyComment}
+            </p>
+            <p>Date : ${date}</p>`
+       
+          
+
+           document.getElementById('history-list').appendChild(hisoryitem)
+          
+
            
             
 
  
-  document.getElementById(addMoneyID).innerText = total.toFixed(2);  
-  document.getElementById(availableMoneyID).innerText = updateMoney.toFixed(2);  
-}
-
-
-
-
-
-function allAroundDonationFunctionn(inputID, addMoneyID, event) {
-  event.preventDefault(); 
-
-  const inputValue = getInputFieldID(inputID);  
-  console.log("Input Value Type:", typeof inputValue);
-
-  addMoneyArraySecond.push(inputValue); 
-  let total = 0
-      for(eachmoneyDonation of addMoneyArraySecond){
-          total+=eachmoneyDonation
-          console.log(total);
-        ;
-      
-       
-      }
-
   
-
- 
- 
-
- 
-  document.getElementById(addMoneyID).innerText = total.toFixed(2);  
- 
 }
+
+
+
 
 
 document.getElementById('donateBtn').addEventListener('click', function(event) {
-  allAroundDonationFunction('inputvalue', 'availablemoney', 'addMoney', event);
+  allAroundDonationFunction('inputvalue', 'availablemoney', 'addMoney', 'fristtext',event,addMoneyArrayFrist);
 });
 
 
 document.getElementById('donateBtnSecond').addEventListener('click', function(event) {
-  allAroundDonationFunctionn('inputvalueSecond',  'addMoneySecond', event);
+  allAroundDonationFunction('inputvalueSecond', 'availablemoney', 'addMoneySecond', 'secondtext',event,addMoneyArraySecond);
+});
+
+document.getElementById('donateBtnthird').addEventListener('click', function(event) {
+  allAroundDonationFunction('inputvaluethird', 'availablemoney', 'addMoneythird','thirdtext', event,addMoneyArrayThird);
 });
 
 
@@ -101,6 +92,7 @@ topHistoryBtn.addEventListener('click',function(){
   topHistoryBtn.classList.add('border-none', 'bg-[#B4F461]', 'text-[#111111]')
   topDonationBtn.classList.remove('border-none', 'bg-[#B4F461]', 'text-[#111111]')
   document.getElementById('donation-form').classList.add('hidden')
+  document.getElementById('history-list').classList.remove('hidden')
   
 
 })
@@ -110,6 +102,8 @@ topDonationBtn.addEventListener('click',function(){
   topHistoryBtn.classList.remove('border-none', 'bg-[#B4F461]', 'text-[#111111]')
 
   document.getElementById('donation-form').classList.remove('hidden')
+  document.getElementById('history-list').classList.add('hidden')
+ 
 
 
 
